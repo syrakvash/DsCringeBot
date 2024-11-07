6e6851b8cd399247f67abc6fa48980503012f730
syrakvash <117604072+syrakvash@users.noreply.github.com>
2024-11-07 13:54:59 +0100

    feat(history): Add requests history, acringe command, small refactor (#11)

--- audio_gen.py
+++ audio_gen.py
@@ -3,7 +3,8 @@ import os
 
 from ai_gen import PATTERNS
 
-MP3_CRINGE_DETECT = 'crigneDetect_output.mp3'
+MP3_CRINGE = 'crigne_output.mp3'
+MP3_ACRINGE = 'acrigne_output.mp3'
 MP3_GREETING = 'greeting_output.mp3'
 MP3_BANNED = 'banned.mp3'
 MP3_FOLDER = 'mp3_temp'
@@ -17,12 +18,15 @@ def generate_audio_from_text(pattern, text):
 def __get_file_name__(pattern):
     cwd = os.getcwd()
     mp3_temp_dir = os.path.join(cwd, MP3_FOLDER)
-    if pattern == PATTERNS.Greeting:
-        mp3_file_name = MP3_GREETING
-    if pattern == PATTERNS.CringeDetect:
-        mp3_file_name = MP3_CRINGE_DETECT
-    if pattern == PATTERNS.Banned:
-        mp3_file_name = MP3_BANNED
+    match pattern:
+        case PATTERNS.Greeting:
+            mp3_file_name = MP3_GREETING
+        case PATTERNS.Cringe:
+            mp3_file_name = MP3_CRINGE
+        case PATTERNS.ACringe:
+            mp3_file_name = MP3_ACRINGE
+        case PATTERNS.Banned:
+            mp3_file_name = MP3_BANNED
     mp3_file_name = os.path.join(mp3_temp_dir, mp3_file_name)
     if not os.path.exists(mp3_temp_dir):
         os.mkdir(mp3_temp_dir)
--- member_data.py
+++ member_data.py
@@ -2,8 +2,8 @@ from threading import Lock
 import time
 import discord
 
-__CRINGE_REQ_COUNT__ = 3
-__CRINGE_REQ_TIMEOUT__ = 80
+__REQ_COUNT__ = 4
+__REQ_TIMEOUT__ = 100
 
 class SingeltonMeta():
     _instances = {}
@@ -22,16 +22,15 @@ class MembersInVoiceData(SingeltonMeta):
     def get_members_dict(self):
         return self.members_dict
 
+    def __init_member__(self, member: discord.Member):
+        self.members_dict[member.id] = {'member': member}
+        self.members_dict[member.id]['reqs_history'] = []
+        self.__reset_request_permission__(member.id)
+
     def add_member(self, member: discord.Member):
         member_to_add = self.members_dict.get(member.id, None)
         if not member_to_add:
-            self.members_dict[member.id] = {'member': member}
-
-    def get_member(self, member: discord.Member):
-        member_to_return = self.members_dict.get(member.id, None)
-        if member_to_return:
-            return member_to_return['member']
-        return None
+            self.__init_member__(member)
 
     def update_member(self, member: discord.Member):
         member_to_update = self.members_dict.get(member.id, None)
@@ -43,26 +42,35 @@ class MembersInVoiceData(SingeltonMeta):
         if member_to_remove:
             del self.members_dict[member.id]
 
-    def cringe_request(self, member:discord.Member):
+    def get_member_request_history(self, member: discord.Member):
+        member_to_get_history = self.members_dict.get(member.id, None)
+        return member_to_get_history['reqs_history']
+    
+    def update_member_request_history(self, member: discord.Member, request, response):
+        member_to_update_history = self.members_dict.get(member.id, None)
+        member_to_update_history['reqs_history'] += [
+            {'role': 'user', 'content': request},
+            {'role': 'assistant', 'content': response}
+            ]
+
+    def check_request_permission(self, member:discord.Member):
         member_to_cringe_request = self.members_dict.get(member.id, None)
         if member_to_cringe_request:
-            if 'cringe_req_count' not in self.members_dict[member.id]:
-                self.__reset_cringe_request__(member.id)
-            else:
-                self.members_dict[member.id]['cringe_req_count'] += 1
-            get_delta_time = time.time() - self.members_dict[member.id]['cringe_first_time']
-            cringe_req_count = self.members_dict[member.id]['cringe_req_count']
-            if cringe_req_count >= __CRINGE_REQ_COUNT__:
-                if get_delta_time < __CRINGE_REQ_TIMEOUT__:
-                    self.members_dict[member.id]['cringe_allowed'] = False
-                    print(f'Member {member.id} banned, cringe reqs number: {cringe_req_count}')
+            if self.members_dict[member.id]['req_count'] == 0:
+                self.members_dict[member.id]['req_first_time'] = time.time()
+            self.members_dict[member.id]['req_count'] += 1
+            get_delta_time = time.time() - self.members_dict[member.id]['req_first_time']
+            req_count = self.members_dict[member.id]['req_count']
+            if req_count >= __REQ_COUNT__:
+                if get_delta_time < __REQ_TIMEOUT__:
+                    self.members_dict[member.id]['req_permission'] = False
+                    print(f'Member {member.id} banned, reqs count: {req_count}')
                 else:
-                    self.__reset_cringe_request__(member.id)
-                
-            return self.members_dict[member.id]['cringe_allowed']
+                    self.__reset_request_permission__(member.id)
+            return self.members_dict[member.id]['req_permission']
         
-    def __reset_cringe_request__(self, member_id):
-        self.members_dict[member_id]['cringe_req_count'] = 0
-        self.members_dict[member_id]['cringe_first_time'] = time.time()
-        self.members_dict[member_id]['cringe_allowed'] = True
+    def __reset_request_permission__(self, member_id):
+        self.members_dict[member_id]['req_count'] = 0
+        self.members_dict[member_id]['req_first_time'] = time.time()
+        self.members_dict[member_id]['req_permission'] = True
         print(f'Member {member_id} initialised/released from ban')
