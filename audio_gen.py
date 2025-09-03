814c9658b1aa69e0fb46ee9a2eea674b09f3c8c4
Yan Syrakvash <yan.syrakvash.consultant@nielsen.com>
2025-09-04 01:52:17 +0200

    cmd

--- audio_gen.py
+++ audio_gen.py
@@ -8,7 +8,8 @@ mp3_file_name_dict = {
     Patterns.ACRINGE: 'acringe_output.mp3',
     Patterns.CLMR: 'clmbr_output.mp3',
     Patterns.GREETING: 'greeting_output.mp3',
-    Patterns.BANNED: 'banned.mp3'
+    Patterns.BANNED: 'banned.mp3',
+    Patterns.STICK: 'stick.mp3',
 }
 
 MP3_FOLDER = 'mp3_temp'
