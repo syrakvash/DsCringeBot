f42497af103fff1df7ab43425352cd9a3973a7a5
Yan Syrakvash <yan.syrakvash.consultant@nielsen.com>
2026-01-30 17:41:19 +0100

    fix

--- audio_gen.py
+++ audio_gen.py
@@ -3,28 +3,18 @@ import os
 
 from ai_gen import Patterns
 
-mp3_file_name_dict = {
-    Patterns.CRINGE: 'cringe_output.mp3',
-    Patterns.ACRINGE: 'acringe_output.mp3',
-    Patterns.CLMBR: 'clmbr_output.mp3',
-    Patterns.GREETING: 'greeting_output.mp3',
-    Patterns.BANNED: 'banned.mp3',
-    Patterns.STICK: 'stick.mp3',
-    Patterns.CLEAN: 'clean.mp3',
-}
-
 MP3_FOLDER = 'mp3_temp'
 
 def generate_audio_from_text(pattern, text):
-    mp3_file_name = __get_file_name__(pattern)
+    mp3_file_name = __get_file_location_name__(pattern)
     res = gTTS(text=text, lang='ru')
-    res.save(mp3_file_name)
+    res.save(__generate_file_name__(pattern))
     return mp3_file_name
 
-def __get_file_name__(pattern):
+def __get_file_location_name__(pattern):
     cwd = os.getcwd()
     mp3_temp_dir = os.path.join(cwd, MP3_FOLDER)
-    mp3_file_name = mp3_file_name_dict[pattern]
+    mp3_file_name = __generate_file_name__(pattern)
     mp3_file_name = os.path.join(mp3_temp_dir, mp3_file_name)
     if not os.path.exists(mp3_temp_dir):
         os.mkdir(mp3_temp_dir)
@@ -32,3 +22,6 @@ def __get_file_name__(pattern):
         if os.path.exists(mp3_file_name):
             os.remove(mp3_file_name)
     return mp3_file_name
+
+def __generate_file_name__(pattern: Patterns):
+    return f"{pattern.name}.mp3"
